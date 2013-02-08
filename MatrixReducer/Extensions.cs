d5366e43ad0a58498e35c6b50a401a4f08e5ce4c
SND\mirhagk_cp <SND\mirhagk_cp@dd3d63e4-b46e-4461-a8c4-23e16edf1824>
2013-02-08 01:13:22 +0000

    Fixed issue with weird folder mappings
    git-svn-id: https://matrixreducer.svn.codeplex.com/svn@23886 dd3d63e4-b46e-4461-a8c4-23e16edf1824

---
+++ MatrixReducer/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrixReducer
+{
+    public static class Extensions
+    {
+        public static Matrix ReadMatrix(this System.IO.TextReader reader)
+        {
+            string result = reader.ReadLine();
+            int numRows = int.Parse(result.Split(' ')[0]);
+            for (int i = 0; i < numRows; i++)
+            {
+                result += "\n" + reader.ReadLine();
+            }
+            return Matrix.FromString(result);
+        }
+        public static Vector ReadVector(this System.IO.TextReader reader)
+        {
+            string[] input = reader.ReadLine().Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+            Vector result = new Vector(input.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i+1] = double.Parse(input[i]);
+            }
+            return result;
+        }
+        public static string RepeatString(this string toRepeat, int numTimes)
+        {
+            string result = "";
+            for (int i = 0; i < numTimes; i++)
+                result += toRepeat;
+            return result;
+        }
+    }
+}
